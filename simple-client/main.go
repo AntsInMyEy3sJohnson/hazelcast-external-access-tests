262b46986edee18a8f0bb0692ee92e0e673d1167
AntsInMyEy3sJohnson <48876328+AntsInMyEy3sJohnson@users.noreply.github.com>
2024-01-29 21:37:59 +0100

    Introduce simple application for accessing Hazelcast cluster.

--- simple-client/main.go
+++ simple-client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"github.com/hazelcast/hazelcast-go-client"
+	"time"
+)
 
 func main() {
 	fmt.Println("hello, hazelcast world!")
+
+	config := hazelcast.Config{}
+	cc := &config.Cluster
+
+	cc.Network.SetAddresses("192.168.44.128")
+	cc.Discovery.UsePublicIP = true
+	ctx := context.TODO()
+
+	client, err := hazelcast.StartNewClientWithConfig(ctx, config)
+
+	if err != nil {
+		panic(err)
+	}
+
+	m, err := client.GetMap(ctx, "awesome-map")
+	if err != nil {
+		panic(err)
+	}
+
+	if size, err := m.Size(ctx); err != nil {
+		if size > 0 {
+			fmt.Println("clearing map")
+			_ = m.Clear(ctx)
+		}
+	}
+
+	for i := 0; ; i++ {
+		key := fmt.Sprintf("mykey-%d", i)
+		value := fmt.Sprintf("myvalue-%d", i)
+		fmt.Printf("writing key-value pair: %s / %s\n", key, value)
+
+		err = m.Set(ctx, key, value)
+		if err != nil {
+			fmt.Printf("set unsuccessful with error: %v\n", err)
+		}
+
+		time.Sleep(500 * time.Millisecond)
+	}
+
 }
